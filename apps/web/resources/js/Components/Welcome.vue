21700ebfb6a59d592dd64166506f7904c4b16580
Túlio Siqueira <djtulioo@gmail.com>
2025-10-07 13:47:53 -0300

    description

--- apps/web/resources/js/Components/Welcome.vue
+++ apps/web/resources/js/Components/Welcome.vue
@@ -12,10 +12,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
             </h1>
 
             <p class="mt-6 text-gray-500 leading-relaxed">
-                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
+                Pulsar é uma plataforma de ouvidoria inteligente que conecta cidadãos, clientes ou colaboradores diretamente a quem pode ouvir e resolver suas demandas. Por meio de um chatbot conversacional, o sistema coleta relatos, organiza feedbacks e identifica padrões de comportamento ou insatisfação em tempo real. Voltado a empresas, instituições públicas e organizações sociais, o Pulsar transforma escuta em inteligência — ajudando líderes e gestores a compreender o que realmente está pulsando em sua comunidade.
             </p>
         </div>
 
