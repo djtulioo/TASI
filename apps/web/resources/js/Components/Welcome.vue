66c4c7cc963564a2a198e0d727f4af5dccc58105
Túlio Siqueira <djtulioo@gmail.com>
2025-10-09 16:13:14 -0300

    logo

--- apps/web/resources/js/Components/Welcome.vue
+++ apps/web/resources/js/Components/Welcome.vue
@@ -5,7 +5,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 <template>
     <div>
         <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-            <!-- <ApplicationLogo class="block h-12 w-auto" /> -->
+            <ApplicationLogo class="block h-12 w-auto" />
 
             <h1 class="mt-8 text-2xl font-medium text-gray-900">
                 Bem vindo ao Pulsar Político!
